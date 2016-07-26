5f51d01969f66e2ca9ea19b0fa2d8b7f5a3098cd
Shawn <214118699@qq.com>
2016-07-26 21:46:55 +0800

    init rote

---
+++ src/route.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "net/http"
+    "log"
+    "html/template"
+)
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path == "/" {
+        http.Redirect(w, r, "/login/index", http.StatusFound)
+    }
+
+    t, err := template.ParseFiles("template/html/404.html")
+    if (err != nil) {
+        log.Println(err)
+    }
+    t.Execute(w, nil)
+}
